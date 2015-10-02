3d6994180885cbb32f5ede8377bb96c722e9aea2
Robert_Romano&Seung_Park <Robert_Romano&Seung_Park@app.academy.io>
2015-10-02 13:59:43 -0400

    move neighbors method to tiles from game

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,17 +4,7 @@ require 'byebug'
 
 class Minesweeper
 
-  MOVES = [
-    ( 1, 1),
-    (-1,-1),
-    ( 1,-1),
-    (-1, 1),
-    ( 0, 1),
-    ( 0,-1),
-    ( 1, 0),
-    (-1, 0)
-  ]
-
+  attr_accessor :board
 
   def initialize
     @board=Board.new
@@ -30,32 +20,33 @@ class Minesweeper
     self.board[pos].flag
   end
 
-  def neighbors(pos)
-    new_coord = []
 
-    MOVES.each do |diff|
-      new_coord << [pos[0] + diff[0], pos[1] + diff[1] ]
-    end
-
-    new_coord.select {|x| x[0].between?(0, self.board.length - 1) && x[1].between?(0, self.board.length - 1) }
-    neighbors = []
-    new_coord.each { |pos| neighbors << self.board[pos] }
-    neighbors
-  end
 
 end
 
 
+
+
 class Tile
 
-
+  MOVES = [
+    [ 1, 1],
+    [-1,-1],
+    [ 1,-1],
+    [-1, 1],
+    [ 0, 1],
+    [ 0,-1],
+    [ 1, 0],
+    [-1, 0]
+  ]
 
   attr_accessor :bombed
 
-  def initialize
+  def initialize(board)
     @revealed = false
     @bombed = false
     @flagged = false
+    @board = board
   end
   #
   # def inspect
@@ -70,6 +61,31 @@ class Tile
     self.flagged = true
   end
 
+  def bombed?
+    self.bombed
+  end
+
+  def set_bomb
+    self.bombed = true
+  end
+
+  def neighbors(pos)
+    new_coord = []
+
+    MOVES.each do |diff|
+      new_coord << [pos[0] + diff[0], pos[1] + diff[1] ]
+    end
+
+    new_coord.select! {|x| x[0].between?(0, self.board.grid.length - 1) && x[1].between?(0, self.board.grid.length - 1) }
+    neighbors = []
+    new_coord.each { |pos| neighbors << self.board[pos] }
+    neighbors
+  end
+
+  def neighbor_bomb_count(pos)
+    neighbors(pos).count{|x| x.bombed == true}
+  end
+
 end
 
 
@@ -81,12 +97,12 @@ class Board
   attr_accessor :grid
 
   def initialize
-    @grid = Array.new(BOARD_SIZE) {Array.new(BOARD_SIZE) {Tile.new}}
+    @grid = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE) {Tile.new(self)} }
 
-    until self.grid.flatten.count{|x| x.bombed == true} == 10
+    until tiles.count { |x| x.bombed? } == 10
       x,y = rand(self.grid.length), rand(self.grid.length)
       pos = [x,y]
-      self[pos].bombed = true
+      self[pos].set_bomb
     end
 
   end
@@ -94,6 +110,10 @@ class Board
   def [](pos)
     row, col = pos[0], pos[1]
     self.grid[row][col]
+  end
+
+  def tiles
+    self.grid.flatten
   end
 
 
